1c2e9e65c24ea0b6a286b62fefb3b459a4bbbf59
Son Do <dominhson580@gmail.com>
2022-08-19 21:27:14 +0700

    Create function renderProjects

--- src/projectDisplay.js
+++ src/projectDisplay.js
@@ -2,16 +2,18 @@ import { getProjects, addProject } from './projectLogic'
 
 const projectsUl = document.querySelector('#projects ul');
 
-const allProjectsLi = document.createElement('li');
-allProjectsLi.classList.add('active');
-allProjectsLi.textContent = "All";
-
-projectsUl.appendChild(allProjectsLi);
-
-getProjects().forEach(project => {
-    const projectLi = document.createElement('li');
-    projectLi.textContent = project;
-    projectsUl.appendChild(projectLi);
-});
+const renderProjects = () => {
+    const allProjectsLi = document.createElement('li');
+    allProjectsLi.classList.add('active');
+    allProjectsLi.textContent = "All";
+    
+    projectsUl.appendChild(allProjectsLi);
+    
+    getProjects().forEach(project => {
+        const projectLi = document.createElement('li');
+        projectLi.textContent = project;
+        projectsUl.appendChild(projectLi);
+    });
+}
 
 export default projectsUl;
