62e212d2188c7153b695089c7532f75f53ceac4a
Son Do <dominhson580@gmail.com>
2022-08-20 22:45:21 +0700

    Create function to render a single task

--- src/task/taskDisplay.js
+++ src/task/taskDisplay.js
@@ -1,2 +1,21 @@
 import { getAllTasks } from "./taskLogic";
 
+const unCompletedTasks = document.querySelector('.taskList[data-status="doing"]');
+const completedTasks = document.querySelector('.taskList[data-status="completed"]');
+const tasks = getAllTasks();
+
+function renderTask(task) {
+    return `
+<div class="task">
+    <div class="text">
+        <p class="title">${task.title}</p>
+        <p>Due Date: <span class="duedate">${task.dueDate}</span></p>
+    </div>
+    <div class="buttons">
+        <button class="secondary">Edit</button>
+        <button class="danger">Delete</button>
+    </div>
+</div>
+`
+}
+
