126f4fbfced4552c2a8335c7536e1f09ecb1d1ac
Son Do <dominhson580@gmail.com>
2022-08-20 15:11:23 +0700

    Add new project to projects array
    Using the imported function addProject

--- src/projectForm.js
+++ src/projectForm.js
@@ -3,17 +3,17 @@ import { addProject } from './projectLogic'
 const showProjectFormButton = document.querySelector('button[data-function="showProjectForm"]');
 showProjectFormButton.addEventListener('click', _showProjectForm);
 
+const addProjectButton = document.querySelector('button[data-function="addProject"]');
+addProjectButton.addEventListener('click', _addProject);
+
 function _showProjectForm() {
     const projectForm = document.querySelector('#projectForm');
     projectForm.parentNode.style.display = 'flex';
 }
 
-const addProjectButton = document.querySelector('button[data-function="addProject"]');
-addProjectButton.addEventListener('click', _addProject);
-
 function _addProject() {
-    const projectName = document.querySelector('#projectName').value;
-    console.log('projectName');
+    const projectName = document.querySelector('input#projectName').value;
+    addProject(projectName);
 }
 
 export { showProjectFormButton, addProjectButton }
