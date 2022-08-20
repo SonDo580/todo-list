3d35f58435ef0b8bd54a95de42434d77be2fff0a
Son Do <dominhson580@gmail.com>
2022-08-20 16:25:36 +0700

    Add some default tasks

---
+++ src/task/taskLogic.js
@@ -0,0 +1,29 @@
+const taskList = [
+    {
+        title: 'Create artificial neuron links',
+        project: 'Mega Mind',
+        description: 'Random description',
+        dueDate: '12-03-2023',
+        note: '',
+        priority: 1,
+        done: false,
+    },
+    {
+        title: 'Swim with baby white shark',
+        project: 'Shark Taming',
+        description: 'Random description',
+        dueDate: '24-12-2024',
+        note: '',
+        priority: 2,
+        done: false,
+    },
+    {
+        title: 'Invent laser canon model A',
+        project: 'Invade Mars',
+        description: 'Random description',
+        dueDate: '14-10-2025',
+        note: '',
+        priority: 3,
+        done: true,
+    }
+];
