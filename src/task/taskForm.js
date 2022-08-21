68cc175d1ed5aa33757f9e0611c5ea7759bfe0bb
Son Do <dominhson580@gmail.com>
2022-08-21 16:06:51 +0700

    Hide taskForm when user click Cancel

--- src/task/taskForm.js
+++ src/task/taskForm.js
@@ -1,10 +1,17 @@
 const showTaskFormButton = document.querySelector('button[data-function="showTaskForm"]');
 showTaskFormButton.addEventListener('click', _showTaskForm);
 
+const cancelTaskFormButton = document.querySelector('button[data-function="cancelTaskForm"]');
+cancelTaskFormButton.addEventListener('click', _hideTaskForm);
+
 const taskForm = document.querySelector('#taskForm');
 
 function _showTaskForm() {
     taskForm.parentNode.style.display = 'flex';
 }
 
-export { showTaskFormButton }
+function _hideTaskForm() {
+    projectForm.parentNode.style.display = 'none';
+}
+
+export { showTaskFormButton, cancelTaskFormButton }
