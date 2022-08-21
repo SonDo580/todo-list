b4387e828c18137713a676c038bb66e34b649a28
Son Do <dominhson580@gmail.com>
2022-08-21 16:25:31 +0700

    Select taskForm input fields

--- src/task/taskForm.js
+++ src/task/taskForm.js
@@ -5,6 +5,13 @@ const cancelTaskFormButton = document.querySelector('button[data-function="cance
 cancelTaskFormButton.addEventListener('click', _hideTaskForm);
 
 const taskForm = document.querySelector('#taskForm');
+const title = document.querySelector('#title');
+const project = document.querySelector('#project');
+const description = document.querySelector('#description');
+const note = document.querySelector('#note');
+const priority = document.querySelector('#priority');
+const doingRadio = document.querySelector('#doing');
+const completedRadio = document.querySelector('#completed');
 
 function _showTaskForm() {
     taskForm.parentNode.style.display = 'flex';
