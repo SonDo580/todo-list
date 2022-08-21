6761e217463bc192104ab868ad0e4e36bbe11e2d
Son Do <dominhson580@gmail.com>
2022-08-21 15:57:58 +0700

    Show taskForm when click addTask button

---
+++ src/task/taskForm.js
@@ -0,0 +1,8 @@
+const showTaskFormButton = document.querySelector('button[data-function="showTaskForm"]');
+showTaskFormButton.addEventListener('click', _showTaskForm);
+
+const taskForm = document.querySelector('#taskForm');
+
+function _showTaskForm() {
+    taskForm.parentNode.style.display = 'flex';
+}
